70422ad78f9fc77a5795fc2ec42c8ca8be6a45fa
Filip Tammergård <filip@tammergard.se>
2019-01-05 09:22:54 +0100

    Added header logo that links to home and fixed a couple of responsibility problems

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const homeLink = document.querySelector(".home-link");
 
 // Set initial state of menu
 let showMenu = false;
@@ -17,6 +18,7 @@ function toggleMenu() {
 		menuNav.classList.add("show");
 		menuBranding.classList.add("show");
 		navItems.forEach(item => item.classList.add("show"));
+		homeLink.classList.add("close");
 
 		// Set menu state
 		showMenu = true;
@@ -26,6 +28,7 @@ function toggleMenu() {
 		menuNav.classList.remove("show");
 		menuBranding.classList.remove("show");
 		navItems.forEach(item => item.classList.remove("show"));
+		homeLink.classList.remove("close");
 
 		// Set menu state
 		showMenu = false;
